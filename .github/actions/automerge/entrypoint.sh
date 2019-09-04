2498148fd358ad7101c5f985b31541d39a81d382
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-04 15:41:57 +0200

    debug info

--- .github/actions/automerge/entrypoint.sh
+++ .github/actions/automerge/entrypoint.sh
@@ -15,6 +15,9 @@ NEUTRAL_EXIT_CODE=78
 STABLE_BRANCH=master
 DEVELOPMENT_BRANCH=devel
 
+
+
+
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
 # FIXME: check that this is the main repository and not a fork
@@ -26,6 +29,25 @@ git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Merge Action"
 
 set -o xtrace
+
+echo $GITHUB_TOKEN
+echo $HOME
+echo $GITHUB_REF
+echo $GITHUB_SHA
+echo $GITHUB_REPOSITORY
+echo $GITHUB_ACTOR
+echo $GITHUB_WORKFLOW
+echo $GITHUB_HEAD_REF
+echo $GITHUB_BASE_REF
+echo $GITHUB_EVENT_NAME
+echo $GITHUB_WORKSPACE
+echo $GITHUB_ACTION
+echo $GITHUB_EVENT_PATH
+echo $RUNNER_OS
+echo $RUNNER_TOOL_CACHE
+echo $RUNNER_TEMP
+echo $RUNNER_WORKSPACE
+
 
 git fetch origin $STABLE_BRANCH
 git fetch origin $DEVELOPMENT_BRANCH
