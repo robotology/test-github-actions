6d5a177edeb9db65c5d97ce70a2b4f52646b525a
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-04 18:18:24 +0200

    More final adjustments

--- .github/actions/nightly-merge/entrypoint.sh
+++ .github/actions/nightly-merge/entrypoint.sh
@@ -25,14 +25,19 @@ git config --global user.name "GitHub Merge Action"
 
 set -o xtrace
 
-
 git fetch origin $INPUT_STABLE_BRANCH
 git checkout -b $INPUT_STABLE_BRANCH origin/$INPUT_STABLE_BRANCH
-git log -1 --pretty=oneline $INPUT_STABLE_BRANCH
 
 git fetch origin $INPUT_DEVELOPMENT_BRANCH
 git checkout -b $INPUT_DEVELOPMENT_BRANCH origin/$INPUT_DEVELOPMENT_BRANCH
+
+set +o
+echo "Nightly merge is trying to merge the following commit ($INPUT_STABLE_BRANCH):"
+git log -1 --pretty=oneline $INPUT_STABLE_BRANCH
+echo "into ($INPUT_DEVELOPMENT_BRANCH)"
 git log -1 --pretty=oneline $INPUT_DEVELOPMENT_BRANCH
+
+set -o xtrace
 
 if git merge-base --is-ancestor $INPUT_STABLE_BRANCH $INPUT_DEVELOPMENT_BRANCH; then
   echo "No merge is necessary"
