03669b6c35556113993e0767719457feeaeda9c6
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-04 19:00:52 +0200

    Add allow_forks parameter

--- .github/actions/nightly-merge/entrypoint.sh
+++ .github/actions/nightly-merge/entrypoint.sh
@@ -11,13 +11,25 @@ echo "Nightly merge is using the following input:"
 echo "  - stable_branch = '$INPUT_STABLE_BRANCH'"
 echo "  - development_branch = '$INPUT_DEVELOPMENT_BRANCH'"
 echo "  - allow_ff = '$INPUT_ALLOW_FF'"
+echo "  - allow_forks = '$INPUT_ALLOW_FORKS'"
 
 NO_FF="--no-ff"
 if $INPUT_ALLOW_FF; then
   NO_FF=""
 fi
 
-# FIXME: check that this is the main repository and not a fork
+if ! $INPUT_ALLOW_FORKS; then
+  URI=https://api.github.com
+  API_HEADER="Accept: application/vnd.github.v3+json"
+  AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+  pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/$GITHUB_REPOSITORY")
+  if [[ "$(echo "$pr_resp" | jq -r .fork)" != "false" ]]; then
+    echo "Nightly merge action is disabled for forks."
+    exit 0
+  else
+    echo "This is not a fork."
+  fi
+fi
 
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git config --global user.email "actions@github.com"
