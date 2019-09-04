d8c6963e9922b5d474508a0cd120cd73e94c1568
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-04 17:46:32 +0200

    Test input

---
+++ .github/actions/automerge/README.md
@@ -0,0 +1,43 @@
+# gh-action-auto-merge
+
+Automatically merge the stable branch into the development one.
+
+If the merge is not necessary, the action will do nothing.
+If the merge fails due to conflicts, the action will fail, and the repository
+maintainer should perform the merge manually.
+
+## Installation
+
+To enable the action simply create the `.github/workflows/nightly-merge.yml`
+file with the following content:
+
+```yml
+name: 'Nightly Merge'
+
+on:
+  schedule:
+    - cron:  '*/0 0 * * *'
+
+jobs:
+  nightly-merge:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@master
+
+    - name: Nightly Merge
+      uses: robotology/gh-action-nightly-merge@master
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Warnings
+
+This is a work in progress.
+At the moment:
+
+* The stable branch name must be `master`
+* The development branch name must be `devel`
+* The actions will probably start also on forks if the actions are enabled for the forks.
+* The actions will always create a merge commit even if fast-forward could be used (`git merge --no-ff`).
---
+++ .github/actions/automerge/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+# FIXME these should be configurable
+STABLE_BRANCH=master
+DEVELOPMENT_BRANCH=devel
+
+# FIXME: check that this is the main repository and not a fork
+
+echo "https://x-access-token:GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Merge Action"
+
+set -o xtrace
+
+echo "INPUT_STABLE_BRANCH = $INPUT_STABLE_BRANCH"
+echo "INPUT_DEVELOPMENT_BRANCH = $INPUT_DEVELOPMENT_BRANCH"
+echo "INPUT_ALLOW_FF = $INPUT_ALLOW_FF"
+
+git fetch origin $STABLE_BRANCH
+git checkout -b $STABLE_BRANCH origin/$STABLE_BRANCH
+git log -1 --pretty=oneline $STABLE_BRANCH
+
+git fetch origin $DEVELOPMENT_BRANCH
+git checkout -b $DEVELOPMENT_BRANCH origin/$DEVELOPMENT_BRANCH
+git log -1 --pretty=oneline $DEVELOPMENT_BRANCH
+
+if git merge-base --is-ancestor $STABLE_BRANCH $DEVELOPMENT_BRANCH; then
+  echo "No merge is necessary"
+  exit 0
+fi;
+
+# do the merge
+# FIXME "--no-ff should be configurable"
+git merge --no-ff --no-edit $STABLE_BRANCH
+git push --force-with-lease origin $DEVELOPMENT_BRANCH
