8a78d995bc1b4bffde2d04474290cc5f8b701989
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-09-20 14:22:04 +0200

    Test revase action

---
+++ .github/actions/rebase/entrypoint.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -e
+
+# Workaround unitl new Actions support neutral strategy
+# See how it was before: https://developer.github.com/actions/creating-github-actions/accessing-the-runtime-environment/#exit-codes-and-statuses
+NEUTRAL_EXIT_CODE=0
+
+echo
+echo "GITHUB_EVENT_PATH = $GITHUB_EVENT_PATH"
+echo "  content:"
+echo "------"
+cat $GITHUB_EVENT_PATH
+echo "------"
+echo
+
+# skip if no /rebase
+echo "Checking if comment contains '/rebase' command..."
+(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -Fq "/rebase") || exit $NEUTRAL_EXIT_CODE
+
+# skip if not a PR
+echo "Checking if issue is a pull request..."
+(jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit $NEUTRAL_EXIT_CODE
+
+if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
+	echo "This is not a new comment event!"
+	exit $NEUTRAL_EXIT_CODE
+fi
+
+PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+echo "Collecting information about PR #$PR_NUMBER of $REPO_FULLNAME..."
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+URI=https://api.github.com
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+
+pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
+
+BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
+BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
+
+USER_LOGIN=$(echo "$pr_resp" | jq -r .user.login)
+
+user_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+            "${URI}/users/${USER_LOGIN}"
+
+if [[ "$(echo "$pr_resp" | jq -r .rebaseable)" != "true" ]]; then
+	echo "GitHub doesn't think that the PR is rebaseable!"
+	exit 1
+fi
+
+if [[ -z "$BASE_BRANCH" ]]; then
+	echo "Cannot get base branch information for PR #$PR_NUMBER!"
+	echo "API response: $pr_resp"
+	exit 1
+fi
+
+HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
+HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
+
+echo "Base branch for PR #$PR_NUMBER is $BASE_BRANCH"
+
+if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
+	echo "PRs from forks are not supported at the moment."
+	exit 1
+fi
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
+
+git remote add fork https://x-access-token:$GITHUB_TOKEN@github.com/$HEAD_REPO.git
+
+set -o xtrace
+
+# make sure branches are up-to-date
+git fetch origin $BASE_BRANCH
+git fetch fork $HEAD_BRANCH
+
+# do the rebase
+git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
+git rebase origin/$BASE_BRANCH
+
+# push back
+git push --force-with-lease
